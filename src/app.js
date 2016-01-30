9af799a4a93b144c5e4bb5a073ddab781a44ffd3
Andrew C <sky713@users.noreply.github.com>
2016-01-29 18:00:53 -0800

    It really works!

--- src/app.js
+++ src/app.js
@@ -11,13 +11,38 @@ var ajax = require('ajax');
 //var channelName = 'twitch';
 var URL = 'https://api.twitch.tv/kraken/streams/';
 
+//counter for top streams
+var top = 0;
+
 // Create a Card with title and subtitle
 var card = new UI.Card({
   title:'Twitch Channels',
   subtitle:'Fetching...'
 });
 
-// Make the request
+//call the thing
+display();
+
+
+card.on('click', 'down', function() {
+        console.log("clicked down");
+        top++;
+        if(top > 24){top = 0};
+        display();
+        });
+
+card.on('click', 'up', function() {
+        console.log("clicked down");
+        top--;
+        if(top < 0){top = 24};
+        display();
+        });
+    
+//This calls twitch and displays the channel at the 'top'
+//displays rank, name, game, and viewers
+function display(){
+  console.log("Running Display for " + top);
+  // Make the request
 ajax(
   {
     url: URL,
@@ -27,8 +52,8 @@ ajax(
     // Success!
     console.log('Successfully fetched twitch data!');
     //update the info
-    card.title(data.streams[0].channel.name)
-    //card.subtitle(data.streams[0].channel.logo);
+    card.title((top + 1) + ". " + data.streams[top].channel.name);
+    card.subtitle(data.streams[top].game + '\nViewers: ' + data.streams[top].viewers);
     
   },
   function(error) {
@@ -36,8 +61,7 @@ ajax(
     console.log('Failed fetching twitch data: ' + error);
     card.subtitle('We didnt do it :(');
   }
-);
-
-
-// Display the Card
-card.show();
+  );
+  // Display the Card
+  card.show();
+}
