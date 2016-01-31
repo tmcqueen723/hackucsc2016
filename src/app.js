1b31085628bf7dae5a91d40bd1f542e05d2ac3a8
keclee95 <keclee@ucsc.edu>
2016-01-31 11:08:15 -0800

    SAVE US

--- src/app.js
+++ src/app.js
@@ -6,15 +6,17 @@
  *              UCSC 2016 Hackathon.
  */
 
-//https://www.dropbox.com/s/fpqsz9nccjkq3l0/chat.json?dl=0
-
 var UI = require('ui');
 var ajax = require('ajax');
 
 // Construct URL
 //var channelName = 'twitch';
 var URL = 'https://api.twitch.tv/kraken/streams/';
-
+//var URLCHAT = 'https://www.dropbox.com/s/fpqsz9nccjkq3l0/chat.json?raw=1';
+//var URLCHAT = 'https://www.dropbox.com/s/fpqsz9nccjkq3l0/chat.json?dl=1';
+//var URLCHAT = 'https://dl.dropboxusercontent.com/content_link/ZN5HEHpI6QoVuif0FyEb8Fd5lGVUMq5vv96fnaUaORtE1gFpw6QFjsp5daQ0CZV5/file/';
+var URLCHAT = 'http://pastebin.com/raw/4hhNRKCq';
+//var URLCHAT = 'http://localhost:8080/chat.json';
 //counter for top streams
 var top = 0;
 
@@ -37,10 +39,14 @@ card.on('click', 'up', function() {
     
 //On middle button press, return to base.
 card.on('click', 'select', function() {
+  console.log("click select");
+  chatDisplay();
+});
+/*card.on('click', 'select', function() {
         console.log("clicked select");
         top = 0;
         display();
-        });
+        });*/
 
 //On bottom button press, next card
 card.on('click', 'down', function() {
@@ -50,6 +56,30 @@ card.on('click', 'down', function() {
         display();
         });
 
+function chatDisplay(){
+  console.log("Running Display for chat");
+  ajax(
+    {
+      url: URLCHAT,
+      type: 'json'
+    },
+  function(data) {
+    // Success!
+    console.log('Successfully fetched twitch chat!');
+    //update the info
+    card.title(data.user);
+    card.body(data.text);
+  },
+  function(error) {
+    // Failure!
+    console.log('Failed fetching twitch chat:' + error);
+    card.subtitle('We failed chat:(');
+  }
+  );
+  // Display the Card
+  card.show();
+  }
+  
 //This calls twitch and displays the channel at the 'top'
 //displays rank, name, game, and viewers
 function display(){
@@ -71,7 +101,7 @@ ajax(
   function(error) {
     // Failure!
     console.log('Failed fetching twitch data: ' + error);
-    card.subtitle('We didnt do it :(');
+    card.subtitle('We didnt twitch :(');
   }
   );
   // Display the Card
